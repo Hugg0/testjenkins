987c3083c5f1f24d6e4dae39ce2551d5a2a2f6c2
Hugg0 <hrsaezc@gmail.com>
2018-04-16 15:25:53 -0300

    Pmmba
    Nuevos cambios
    
    Co-Authored-By: hugg0 <hugg0@users.noreply.github.com>

---
+++ src/HolaMundo.java
@@ -0,0 +1,10 @@
+/**
+ * Hola mundo en Java
+ * 
+ * @author Hugg0
+ */
+public class HolaMundo {
+  public static void main (String[] args) {
+    System.out.println(naranja + "Hola" + ":)" + ":D" + ":)" +" Mundo");
+  }
+}
